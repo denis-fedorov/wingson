042b47921637b71ef4cd9b7b48aaadd953de885f
Denis Fedorov <denis_fedorov@epam.com>
2023-03-20 18:13:35 +0400

    Add the SharedKernel project

---
+++ source/src/WingsOn.SharedKernel/NullGuard.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace WingsOn.SharedKernel;
+
+public static class NullGuard
+{
+    public static T ThrowIfNull<T>(T obj, [CallerArgumentExpression("obj")] string? paramName = null)
+    {
+        if (obj is null)
+        {
+            throw new ArgumentNullException(paramName);
+        }
+
+        return obj;
+    }
+}
